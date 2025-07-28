adf4e3e62c3b7a68e7766f9ac160708a3b0eace4
capac <capac@users.noreply.github.com>
2025-07-28 10:37:57 +0100

    Working MLflow-registered best XGBoost model
    Best XGBoost model on MLflow used for prediction with testing script

---
+++ mlflow-web-service/predict.py
@@ -0,0 +1,46 @@
+import pickle
+import mlflow
+import pandas as pd
+from mlflow.tracking import MlflowClient
+from flask import Flask, request, jsonify
+
+# best XGBoost model run saved in MLflow
+RUN_ID = 'c078191b22ad48a99521644774aa860b'
+MLFLOW_TRACKING_URI = 'http://127.0.0.1:5000'
+
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
+
+model_uri = f'runs:/{RUN_ID}/artifacts'
+model = mlflow.pyfunc.load_model(model_uri)
+
+path = client.download_artifacts(
+    run_id=RUN_ID,
+    path='best_model/preprocessor.bin'
+    )
+with open(path, 'rb') as f_out:
+    preprocessor = pickle.load(f_out)
+
+
+def predict(X):
+    sr = pd.DataFrame([X])
+    features = preprocessor.transform(sr)
+    pred = model.predict(features)
+    return float(pred[0])
+
+
+app = Flask('higher-education-students-performance-evaluation')
+
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    evaluation = request.get_json()
+    prediction = predict(evaluation)
+
+    result = {'grade': prediction}
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
